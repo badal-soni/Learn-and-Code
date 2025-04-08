073c1e1b918f2bea16f5603bd707e33b7d5cae54
badal.soni <badal.soni@intimetec.com>
2025-04-08 14:10:10 +0530

    moved GlobalExceptionHandler from controller package to exception package

--- blog-application/src/main/java/com/itt/blogapplication/exception/GlobalExceptionHandler.java
+++ blog-application/src/main/java/com/itt/blogapplication/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package com.itt.blogapplication.controller;
+package com.itt.blogapplication.exception;
 
 import com.itt.blogapplication.dto.response.ApiErrorResponse;
-import com.itt.blogapplication.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
