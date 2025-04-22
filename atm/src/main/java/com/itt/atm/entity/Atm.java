8c5212f099e669e3aa63428084e736624988a75a
badal.soni <badal.soni@intimetec.com>
2025-04-22 11:25:46 +0530

    Atm machine REST API assignment

---
+++ atm/src/main/java/com/itt/atm/entity/Atm.java
@@ -0,0 +1,16 @@
+package com.itt.atm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Atm extends BaseEntity {
+
+    private long balance;
+
+}
