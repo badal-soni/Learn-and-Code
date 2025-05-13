88e851661043c4b053ff4632c05006aff3659f0b
badal.soni <badal.soni@intimetec.com>
2025-05-13 18:12:36 +0530

    added negative scenerios

--- src/test/java/com/itt/assignment7/DivisionCheckerTest.java
+++ src/test/java/com/itt/assignment7/DivisionCheckerTest.java
@@ -3,10 +3,13 @@ package com.itt.assignment7;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DivisionCheckerTest {
 
     @Test
-    public void testCountNumbersWithEqualsDivisors() {
+    public void testCountNumbersWithEqualsDivisorsShouldPass() {
         final int[] input = {3, 15, 100};
         final int[] expectedOutput = {1, 2, 15};
         int index = 0;
@@ -14,6 +17,24 @@ public class DivisionCheckerTest {
             final int actualOutput = DivisionChecker.countNumbersWithEqualDivisors(k);
             Assert.assertEquals(expectedOutput[index++], actualOutput);
         }
+    }
+
+    @Test
+    public void testCountNumbersWithEqualDivisorsShouldFail() {
+        final int[] input = {3, 15, 100};
+        final int[] expectedOutput = {1, 2, 14};
+        int index = 0;
+        List<Integer> failed = new ArrayList<>();
+        for (int i = 0; i < input.length; i++) {
+            final int k = input[i];
+            final int actualOutput = DivisionChecker.countNumbersWithEqualDivisors(k);
+            if (actualOutput != expectedOutput[index]) {
+                failed.add(index);
+            } else if (i < input.length - 1) {
+                Assert.assertEquals(expectedOutput[index++], actualOutput);
+            }
+        }
+        Assert.assertFalse(failed.isEmpty());
     }
 
 }
