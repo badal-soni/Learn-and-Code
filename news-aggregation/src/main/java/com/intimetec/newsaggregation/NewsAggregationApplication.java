626585704a2940a9e15040562dfa12bf6da5d4b8
badal.soni <badal.soni@intimetec.com>
2025-07-06 22:00:48 +0530

    Test cases

--- news-aggregation/src/main/java/com/intimetec/newsaggregation/NewsAggregationApplication.java
+++ news-aggregation/src/main/java/com/intimetec/newsaggregation/NewsAggregationApplication.java
@@ -3,10 +3,11 @@ package com.intimetec.newsaggregation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-//@EnableScheduling
+@EnableScheduling
 public class NewsAggregationApplication {
 
     public static void main(String[] args) {
